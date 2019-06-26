d0c12090ace7ea4c5485dddddfe6fedbd561f5d0
brianktran <bkt5031@gmail.com>
2019-06-25 23:10:05 -0400

    csv now outputs an pandas is started

--- Angular-Django-example-Angular-Rest-Client/AngularDjango/src/app/customers-list/customers-list.component.ts
+++ Angular-Django-example-Angular-Rest-Client/AngularDjango/src/app/customers-list/customers-list.component.ts
@@ -3,18 +3,20 @@ import { Observable } from 'rxjs';
 import { CustomerService } from '../customer.service';
 import { Customer } from '../customer';
 import * as saveAs from 'file-saver';
-
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'customers-list',
   templateUrl: './customers-list.component.html',
   styleUrls: ['./customers-list.component.css']
 })
+
 export class CustomersListComponent implements OnInit {
 
   customers: Observable<Customer[]>;
 
   constructor(private customerService: CustomerService) { }
+
 
   ngOnInit() {
     this.reloadData();
@@ -36,11 +38,77 @@ export class CustomersListComponent implements OnInit {
 
 
 
-  getcsvFile() {
+  convertToCSV(data: any, columns: string, header: string, delimiter: string |
+undefined) {
 
-      this.customers = this.customerService.getCustomersList();
-      let file = new Blob([JSON.stringify(this.customers)], { type: 'data:application/csv;charset=utf-8,content_encoded_as_url' });
-      saveAs(file, 'customerList.csv')
-    }
+ let row = '';
+ const del = delimiter ? delimiter : ';';
+ const col = columns.split(del);
+ const head = header.split(del);
+
+
+ // creating the header
+ for (const headerTxt of head) {
+   row += headerTxt + del;
+ }
+ row += '\r\n';
+ //  start with the rows
+ for (const dataset of data) {
+   let line = '';
+   for (let i = 0; i < col.length; i++) {
+     let dataToAdd = dataset[col[i]];
+     if (dataset[col[i]] == null || dataset[col[i]] === undefined) {
+       dataToAdd = '';
+     }
+     line += '"' + dataToAdd + '"' + del;
+   }
+   row += line + '\r\n';
+ }
+ return row;
+}
+
+download(data: any, filename: string, columns: string, header: string, delimiter: string | undefined) {
+  const csvData = this.convertToCSV(data, columns, header, delimiter);
+  const link: any = document.createElement('a');
+  link.setAttribute('style', 'display:none;');
+  document.body.appendChild(link);
+  const blob = new Blob([csvData], {type: 'text/csv'});
+  link.href = window.URL.createObjectURL(blob);
+
+  const isIE = !!(<any> document).documentMode;
+
+  if (isIE) {
+    navigator.msSaveBlob(blob, filename + '.csv');
+  } else {
+    link.download = filename + '.csv';
+  }
+  link.click();
+  link.remove();
+}
+
+//   getcsvFile() {
+//
+//
+//       this.customers = this.customerService.getCustomersList();
+//       let file = new Blob([JSON.stringify(this.customers)], { type: 'data:application/csv;charset=utf-8,content_encoded_as_url' });
+//       this.convertToCSV(file, 'customer',
+// ['id', 'name', 'age', 'active'],
+// ['ID', 'Name', 'Age', 'Active'], ',')
+//       download(file, 'customer',
+// ['id', 'name', 'age', 'active'],
+// ['ID', 'Name', 'Age', 'Active'], ',')
+//
+//     }
+getcsvFile() {
+
+    this.customerService.getCustomersList()
+     .pipe(take(1)) // <-- HERE
+     .subscribe(customers=>{ // <-- AND HERE
+      if (customers) {
+        this.download(customers, 'customer','id,name,age,active','ID,Name,Age,Active', ',');
+      }
+   });
+
+}
 
 }
---
+++ customerStats/customerMeanAge.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+# %matplotlib inline
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+# Read in data into a dataframe
+df = pd.read_csv('customer.csv')
+
+# Show the first 5 (or 10) rows of the table
+df.tail(10)
+
+df = df.iloc[:,:4]
+df.head()
+
+desc = df.describe()
+
+print(desc)
